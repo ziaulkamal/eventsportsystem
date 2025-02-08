14207a65aec46e5984d287b43f95f81785b4687f
ziaulkamal <ziaul.kamal1109@gmail.com>
2025-02-08 15:56:19 +0700

    update

---
+++ pages/atleet/search.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const SearchPage = () => {
+  return (
+    <div>
+      <h1>Search Atleet</h1>
+      {/* Tambahkan komponen atau logika lainnya di sini */}
+    </div>
+  );
+};
+
+export default SearchPage;
